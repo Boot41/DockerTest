acd16080aed92a477d5464e814d01599fbb22fda
jhks777 <jhansi.priya@think41.com>
2024-09-05 16:33:12 +0530

    US-2-BT-1--Develop APIs to support job searching and detailed job information retrieval for candidates.

--- server/app/views.py
+++ server/app/views.py
@@ -17,3 +17,29 @@ class JobListingListView(APIView):
         job_listings = JobListing.objects.filter(employer_id=employer_id)
         serializer = JobListingSerializer(job_listings, many=True)
         return Response(serializer.data)
+
+class JobSearchView(APIView):
+    def get(self, request):
+        keywords = request.query_params.get('keywords', '')
+        location = request.query_params.get('location', '')
+        job_type = request.query_params.get('job_type', '')
+
+        job_listings = JobListing.objects.all()
+        if keywords:
+            job_listings = job_listings.filter(title__icontains=keywords)
+        if location:
+            job_listings = job_listings.filter(location__icontains=location)
+        if job_type:
+            job_listings = job_listings.filter(job_type__icontains=job_type)
+
+        serializer = JobListingSerializer(job_listings, many=True)
+        return Response(serializer.data)
+
+class JobDetailView(APIView):
+    def get(self, request, job_id):
+        try:
+            job_listing = JobListing.objects.get(id=job_id)
+            serializer = JobListingSerializer(job_listing)
+            return Response(serializer.data)
+        except JobListing.DoesNotExist:
+            return Response({'error': 'Job not found'}, status=status.HTTP_404_NOT_FOUND)
