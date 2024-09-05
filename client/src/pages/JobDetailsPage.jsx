ade13f9c48f56016e03fc2c9b42558ec6deda66a
jhks777 <jhansi.priya@think41.com>
2024-09-05 16:44:06 +0530

    US-4-FT-1--Create a job application form component for candidates to submit their application.

--- client/src/pages/JobDetailsPage.jsx
+++ client/src/pages/JobDetailsPage.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Header from './Header';
 import JobDetails from './JobDetails';
+import JobApplicationForm from './JobApplicationForm';
 import Footer from './Footer';
 
 const JobDetailsPage = () => {
-  return (
-    <div className="flex flex-col min-h-screen bg-white text-gray-800">
-      <Header className="sticky top-0 shadow-md z-10" />
-      <main className="flex-grow p-4 md:p-8">
-        <JobDetails />
-      </main>
-      <Footer className="bg-gray-100 shadow-md" />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header className="sticky top-0 shadow-md bg-white" />
+            <div className="flex flex-col md:flex-row flex-1">
+                <div className="w-full md:w-2/3 p-4">
+                    <JobDetails />
+                </div>
+                <div className="w-full md:w-1/3 p-4">
+                    <JobApplicationForm />
+                </div>
+            </div>
+            <Footer className="bg-gray-800 text-white w-full" />
+        </div>
+    );
 };
 
 export default JobDetailsPage;
