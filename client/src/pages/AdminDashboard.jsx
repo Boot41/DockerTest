2c91a0a683d06938683aaa65ebfa3b1011bceb51
jhks777 <jhansi.priya@think41.com>
2024-09-05 16:40:48 +0530

    US-3-FT-3--Implement a compliance alert component to notify admins of non-compliant job postings, prompting necessary actions to address compliance issues.

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -3,19 +3,24 @@ import Header from './Header';
 import AdminJobOverview from './AdminJobOverview';
 import JobStatusUpdate from './JobStatusUpdate';
 import ComplianceAlert from './ComplianceAlert';
+import Footer from './Footer';
 
 const AdminDashboard = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-white">
-      <Header className="sticky top-0 z-10 shadow-md" />
-
-      <main className="flex-grow p-4">
-        <AdminJobOverview className="border border-gray-300 shadow-sm mb-4 p-4" />
-        <JobStatusUpdate className="border border-gray-300 shadow-sm mb-4 p-4" />
-        <ComplianceAlert className="border border-gray-300 shadow-sm mb-4 p-4" />
-      </main>
+    <div className="flex flex-col h-screen bg-navy-900 text-white">
+      <Header />
+      <div className="flex-grow flex flex-col md:flex-row">
+        <div className="flex-grow p-4">
+          <AdminJobOverview />
+          <ComplianceAlert />
+        </div>
+        <div className="flex-grow p-4">
+          <JobStatusUpdate />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
-};
+}
 
 export default AdminDashboard;
